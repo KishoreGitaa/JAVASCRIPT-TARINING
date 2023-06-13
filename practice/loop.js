e1d6b7362c79e71a35fe5f28e242cb8e2d65311c
Kishore <kishore.v@gitaa.in>
2023-06-13 18:26:15 +0530

    Exercises

--- practice/loop.js
+++ practice/loop.js
@@ -1,17 +1,33 @@
-// 0 to 9 
-for (let i=0; i<=9;i++){
-    console.log(i);
-}
-console.log("");
 
-let number = 9 
-let i=0
-while (i <= number){
-    console.log(i);
-    i++
-}
+// // for(let i=1 ; i<=10; i+=5){
+// //     console.log(i);
+// // }
 
-let num = 5
-for (let i=1 ;i <= num; i++){
-    console.log("#".repeat(i));
+// let number =100
+// let i=0;
+// let emety=[]
+// while (i <= number){
+//     console.log(i);
+//     i+=5
+// }
+
+// let num =20
+// let a=0;
+// do{
+//     console.log(a);
+//     a++
+// }
+// while(a<=20)
+
+
+// 1000 / 3
+
+let emety = []
+let number = 1000
+for (let i=1; i<=number; i++){
+    if (i % 3 == 0 ){
+        // console.log(i);
+        emety.push(i)
+    }
 }
+console.log(emety, {'maxArrayLength': null});
